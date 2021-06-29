59f08f5e5cb86b10ed778481d364cd0b91b95cc7
yabby <yabby1997@gmail.com>
2021-06-29 22:51:20 +0900

    refactor : touchesBegan 대신 WKGestureRecognizer를 이용하여 게임플레이가 가능하도록 수정

--- MissileCommander WatchKit Extension/InterfaceController.swift
+++ MissileCommander WatchKit Extension/InterfaceController.swift
@@ -13,8 +13,9 @@ class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var skView: WKInterfaceSKScene!
     
+    let gameScene = GameScene(size: CGSize(width: 600, height: 500))
+    
     override func awake(withContext context: Any?) {
-        let gameScene = GameScene(size: CGSize(width: 600, height: 500))
         gameScene.scaleMode = .aspectFit
         self.skView.presentScene(gameScene)
         self.skView.preferredFramesPerSecond = 60
@@ -27,5 +28,10 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
-
+    
+    @IBAction func handleGesture(gestureRecognizer: WKGestureRecognizer) {
+        let bound = gestureRecognizer.objectBounds()
+        let location = gestureRecognizer.locationInObject()
+        gameScene.touched(location: CGPoint(x: (location.x / bound.maxX) * 600, y: ((bound.maxY - location.y) / bound.maxY) * 500))
+    }
 }
